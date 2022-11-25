17c3ad76d7ecfa5d368892e75315a3fcc3069140
Coldman <ocamara015@gmail.com>
2022-11-25 13:24:09 +0000

    backend link on prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://8438-41-214-14-21.ngrok.io/api/',
-  baseUrl: 'https://8438-41-214-14-21.ngrok.io/'
+  backendUrl: 'https://obscure-dusk-56375.herokuapp.com/api/',
+  baseUrl: 'https://obscure-dusk-56375.herokuapp.com/'
 };
